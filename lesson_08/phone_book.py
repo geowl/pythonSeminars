ab75bae6f5cba5b61a2e71a4570313c666ed41ad
Vladislav <geowl@vk.com>
2023-11-27 10:54:14 +0300

    добавил рабочий код в phone_book

--- lesson_08/phone_book.py
+++ lesson_08/phone_book.py
@@ -13,90 +13,94 @@
 # человека)
 # 4. Использование функций. Ваша программа
 # не должна быть линейной
-
-def show_all():
+def show_all(file_name: str):
     with open(file_name, 'r', encoding='utf-8') as f:
-        data = f.readline()
-        print(''.join(data))
- def remove(file_name: str):
-    last_name = input("Введите фамилию: ")
-    first_name = input("Введите имя: ")
-    patronymic = input("Введите отчество: ")
-    phone_number = input("Введите номер телефона: ")
-   data = ''
-with open(file_name, 'r', encoding='utf-8') as f:
-    data = f.readline()
-    s = (f'{last_name}, {first_name}, {patronymic}, {phone_number}\n')
+        data = f.readlines()
+        print("".join(data))
+
+
+def remove(file_name: str):
+    last_name = input('Введите фамилию: ')
+    first_name = input('Введите имя: ')
+    patronymic = input('Введите отчество: ')
+    phone_number = input('Введите номер телефона: ')
+    data = ""
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+    s = f'{last_name}, {first_name}, {patronymic}, {phone_number}\n'
     data.remove(s)
-with open (file_name, 'w', encoding='utf-8') as f:
-    f.writelines(data)
-def modify(file_name:str):
-# print("Введите данные для поиска:\n")
-# last_name = input('Введите фамилию: ')
-# first_name = input('Введите имя: ')
-# patronymic = input('Введите отчество: ')
-# phone_number = input('Введите номер телефона: ')
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.writelines(data)
 
-old_data = find_by_attribute(file_name, True)
 
-print("Введите новые данные:\n")
-last_name_ = input('Введите фамилию: ')
-first_name_ = input('Введите имя: ')
-patronymic_ = input('Введите отчество: ')
-phone_number_ = input('Введите номер телефона: ')
-data = ""
-with open(file_name, 'r',encoding='utf-8') as f:
-    data = f.readlines()
-i = data.index(old_data)
-data[i] = f'{last_name_}, {first_name_}, {patronymic_}, {phone_number_}\n'
+def modify(file_name: str):
+    old_data = find_by_attribute(file_name, True)
+    print("Введите новые данные:\n")
+    last_name_ = input('Введите фамилию: ')
+    first_name_ = input('Введите имя: ')
+    patronymic_ = input('Введите отчество: ')
+    phone_number_ = input('Введите номер телефона: ')
+    data = ""
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+    i = data.index(old_data)
+    data[i] = f'{last_name_}, {first_name_}, {patronymic_}, {phone_number_}\n'
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.writelines(data)
 
-with open(file_name, 'w',encoding='utf-8') as f:\
-f.writelines(data)
 
-def add_new(file_name : str) -> None:
-    """
-    Функция принимая имя файла(file_name) в виде строчки
-    :param file_name:
-    :return:
-    """
-    # data = input("Введите ФИО и номер телефона через пробел: ")
-    last_name = input("Введите фамилию: ")
-    first_name = input ("Введите имя: ")
-    patronymic = input("Введите отчество: ")
-    phone_number = input("Введите номер телефона: ")
-    # Иванов Петр Сидорович 1234567
-    # Петров Иван Сидорович 7654321
-    # file_name = 'phone_book.txt'
-    # f = open(file_name, 'a', encoding='utf-8')
-    # f.close()
-    with open(file_name, 'a', encoding = 'utf-8') as fd:
+def find_by_attribute(file_name: str, option: bool):
+    c = input("Введите 1 для поиска по фамилии, 2 для поиска по имени")
+    opt = 0
+    if c == '1':
+        opt = 0
+    elif c == '2':
+        opt = 1
+    attr = input("Введите имя/фамилию для поиска")
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+    data = list(filter(lambda x: x.split(', ')[opt] == attr, data))
+    print(list(zip(range(1, len(data) + 1), data)))
+    if option:
+        id = input("Введите id нужного пользователя для изменения данных: ")
+    else:
+        id = input("Введите id нужного пользователя: ")
+    return data[int(id) - 1]
+
+
+def add_new(file_name: str):
+    last_name = input('Введите фамилию: ')
+    first_name = input('Введите имя: ')
+    patronymic = input('Введите отчество: ')
+    phone_number = input('Введите номер телефона: ')
+    with open(file_name, 'a', encoding='utf-8') as fd:
         fd.write(f'{last_name}, {first_name}, {patronymic}, {phone_number}\n')
+
+
 def main():
-    file_name = 'phone_book.txt'
+    file_name = 'phonebook.txt'
     flag_exit = False
     while not flag_exit:
         print('1 - показать все записи')
-        print('2 - добавить запись')
-        print('3 - поиск записи')
-        print('4 - удалить запись')
-        answer = input("Введите операцию или х для вывода: ")
+        print('2 - добавить все запись')
+        print('3 - удалить запись')
+        print('4 - изменить запись')
+        print('5 - поиск записи по имени/фамилии')
+        answer = input('Введите операцию или x для выхода: ')
         if answer == '1':
-            show_all()
+            show_all(file_name=file_name)
         elif answer == '2':
             add_new(file_name)
-        elif answer == "4":
+        elif answer == '3':
             remove(file_name)
+        elif answer == '4':
+            modify(file_name=file_name)
+        elif answer == '5':
+            print(find_by_attribute(file_name, False))
         elif answer == 'x':
             flag_exit = True
 
 
-if __name__ == '__main__':
+if name == 'main':
     main()
-#     print('123')
-#     print(__name__)
-# else:
-#     print('123')
-#     print('отрабатывает else')
-#     print(__name__)
-
 
